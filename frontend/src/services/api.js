ba4a941402d71f8e13280ce4007dcb1ba554b3bd
szoha2095 <szoha2095@gmail.com>
2025-01-03 22:47:25 +0530

    Updated backend render url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:8080/api" });
+const API = axios.create({
+  baseURL: "https://employeeexitmanager.onrender.com/api",
+});
 
 export default API;
